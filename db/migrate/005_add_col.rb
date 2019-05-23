c5e130c124f77b2736667ca38632f05af6ba7494
Chet  Chopra <chopra.chet@gmail.com>
2019-05-23 22:18:26 +0000

    Done.

---
+++ db/migrate/005_add_col.rb
@@ -0,0 +1,6 @@
+class AddCol < ActiveRecord::Migration[5.1]
+  def change
+    add_column :costumes, :created_at, :datetime
+    add_column :costumes, :updated_at, :datetime
+  end
+end
---
+++ db/migrate/006_change_col_name_again.rb
@@ -0,0 +1,7 @@
+class ChangeColNameAgain < ActiveRecord::Migration[5.1]
+  def change
+    
+    rename_column :haunted_houses, :long_description, :description
+    
+  end
+end
