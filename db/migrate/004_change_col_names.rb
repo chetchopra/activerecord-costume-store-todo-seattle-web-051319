1b31b3c90dd6af801d2f9a7a828bdb6be03732cc
Chet  Chopra <chopra.chet@gmail.com>
2019-05-23 22:12:30 +0000

    Automatically backed up by Learn

--- db/migrate/004_change_col_names.rb
+++ db/migrate/004_change_col_names.rb
@@ -2,6 +2,5 @@ class ChangeColNames < ActiveRecord::Migration[5.1]
   def change 
     change_column  :costume_stores, :opening_time, :datetime 
     change_column  :costume_stores, :closing_time, :datetime 
-    
   end
 end
